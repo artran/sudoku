36ba5b51a0b7e242bdcf1094fadaa74c3d930396
Ray Tran <rt@elixirsoftware.co.uk>
2021-04-24 13:25:15 +0100

    Ensure CellGroup only has nine elements

--- sudoku/cell_group.py
+++ sudoku/cell_group.py
@@ -4,8 +4,11 @@ from sudoku.cell import Cell
 
 
 class CellGroup:
-    def __init__(self, cells: Iterable[Cell]):
-        self._cells = cells
+    def __init__(self, cell_values: Iterable[int]):
+        self._cells = [Cell(n) for n in cell_values]
+
+        if len(self._cells) != 9:
+            raise ValueError('Must be nine int values')
 
     def possible(self, candidate: int) -> bool:
         return all(cell.value != candidate for cell in self._cells)
--- sudoku/tests/test_cell_group.py
+++ sudoku/tests/test_cell_group.py
@@ -1,15 +1,25 @@
+from itertools import repeat
 import unittest
 
-from assertpy import assert_that
+from assertpy import assert_that, fail
 
-from sudoku.cell import Cell
 from sudoku.cell_group import CellGroup
 
 
 class CellGroupTestCase(unittest.TestCase):
+    def test_iterator_must_have_9_elements(self):
+        for i in range(12):
+            with self.subTest('Only 9 elements are acceptable', i=i):
+                if i != 9:
+                    assert_that(CellGroup).raises(ValueError).when_called_with(repeat(0, i))
+                else:
+                    try:
+                        CellGroup(repeat(0, i))
+                    except ValueError:
+                        fail('9 elements should be acceptable')
+
     def test_possible_returns_true_if_number_is_possible(self):
-        cells = [Cell(n) for n in range(9)]  # 1 cell is blank and only 9 is missing
-        cell_group = CellGroup(cells)
+        cell_group = CellGroup(range(9))  # 1 cell is blank and only 9 is missing
 
         for i in range(1, 10):
             with self.subTest('Only 9 should be possible', i=i):
