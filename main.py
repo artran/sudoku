bf14b937a1939d2e47350285638e38b3d53ec9e8
Ray Tran <rt@elixirsoftware.co.uk>
2021-04-24 17:32:20 +0100

    Make the `main.py` file executable

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from io import StringIO
 
+from sudoku.board import Board
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    board_file = StringIO('6,,,,,3,,,\n'
+                          ',9,,8,6,7,3,,\n'
+                          ',3,7,2,5,,6,,\n'
+                          ',,4,1,3,,2,,\n'
+                          '3,,9,,,,1,,5\n'
+                          ',,8,,2,9,4,,\n'
+                          ',,5,,4,1,7,2,\n'
+                          ',,3,7,9,6,,8,\n'
+                          ',,,5,,,,,1\n')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    board = Board()
+    board.load_file(board_file)
+    board.solve()
+
+    board.print()
